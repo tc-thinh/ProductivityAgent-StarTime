94cfd289940af6e26f0da6a11d4b1256f1b2adc6
tcthinh <congthinh6304@gmail.com>
2025-02-26 02:54:36 -0700

    Category Manager Page Set up

---
+++ frontend/app/category-management/page.tsx
@@ -0,0 +1,35 @@
+import { AppSidebar } from "@/components/sidebar-components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+import CategoryManagement from "@/components/category-management/category-managmer"
+
+
+export default function Landing() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+        </header>
+        <div className="flex flex-1 flex-col items-center justify-center gap-4 p-4">
+          <CategoryManagement />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
---
+++ frontend/components/category-management/category-managmer.tsx
@@ -0,0 +1,7 @@
+export default function CategoryManagement() {
+    return (
+        <div>
+        <h1>Category Management</h1>
+        </div>
+    )
+}
