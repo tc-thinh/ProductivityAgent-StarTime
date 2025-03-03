3083f7d88d0c59c969672fb20a2dd59f2af95c03
tcthinh <congthinh6304@gmail.com>
2025-03-03 09:58:55 -0700

    WebSocket workflow refinement

--- backend/database_service/consumers/consumers.py
+++ backend/database_service/consumers/consumers.py
@@ -1,5 +1,5 @@
 import json
-from database_service.models import Message
+from database_service.models import Conversation
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -9,6 +9,12 @@ class ConversationUpdatesWebSocket(AsyncWebsocketConsumer):
         self.conversation_id = self.scope['url_route']['kwargs']['conversationId']
         self.group_name = f'Conversation_{self.conversation_id}'
         
+        self.conversation = await sync_to_async(Conversation.objects.filter(c_id=self.conversation_id).first)()
+        if not self.conversation:
+            print(f"Connection rejected: conversation {self.conversation_id} does not exist")
+            await self.close()  # Reject connection
+            return
+
         # Print the section ID
         print(f"Connected to conversation: {self.conversation_id}")
 
@@ -21,18 +27,8 @@ class ConversationUpdatesWebSocket(AsyncWebsocketConsumer):
         # Accept the connection
         await self.accept()
 
-        # Query the database for messages that contain the conversation Id
-        messages = await sync_to_async(list)(Message.objects.filter(c_id=self.conversation_id))
-
-        # Send the prompts to the client
-        for message in messages:
-            await self.send(text_data=json.dumps({
-                'conversation_id': self.conversation_id,
-                'role': message.m_role,
-                'content': message.m_content,
-                'tool_calls': None,
-                'tool_call_id': None
-            }))
+        self.conversation_messages = self.conversation.c_messages  
+        await self.send(text_data=json.dumps(self.conversation_messages))
 
     async def disconnect(self, close_code):
         # Remove the client from the group
@@ -41,17 +37,28 @@ class ConversationUpdatesWebSocket(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        if self.conversation:
+            await sync_to_async(self._update_conversation_messages)()
+
+    def _update_conversation_messages(self):
+        """Sync method to update conversation messages in the database."""
+        self.conversation.c_messages = self.conversation_messages 
+        self.conversation.save()
+
     async def receive(self, text_data):
         # Process the received data (assumed to be JSON)
+        # data {'type': 'conversation_name'/'message', 'data': 'JSON'}
         data = json.loads(text_data)
-        
+        if data.get('type') != 'conversation_name':
+            self.conversation_messages.append(data.get('message'))
+
         # Send the message to the group
         await self.channel_layer.group_send(
             self.group_name,
             {
                 'type': 'chat_message',
                 'conversation_id': self.conversation_id,
-                'message': data.get('message')
+                'message': data
             }
         )
 
