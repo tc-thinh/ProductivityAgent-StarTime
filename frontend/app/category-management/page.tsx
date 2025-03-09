7eeccc8c30029eea1a7d358f2fb9e9067803e323
Dat Nguyen <darrennguyen238@gmail.com>
2025-03-09 15:29:37 -0700

    Dynamic breadcrumb added

--- frontend/app/category-management/page.tsx
+++ frontend/app/category-management/page.tsx
@@ -1,13 +1,6 @@
 "use client"
 
 import { useState, useEffect } from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
 import { toast } from "sonner"
 
 import CategoryManagement from "@/components/category-management/category-manager"
@@ -59,21 +52,6 @@ export default function Landing() {
 
   return (
     <>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <div className="flex items-center gap-2 px-4">
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="/category-management">
-                  Category Manager
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-
       <div className="p-4 border-r border-gray-200 overflow-y-auto">
         <CategoryManagement
           saveEdit={handleSave}
---
+++ frontend/components/dynamic-breadcrumb/dynamic-breadcrumb.tsx
@@ -0,0 +1,51 @@
+// components/dynamic-breadcrumb.tsx
+"use client"; // Mark as a Client Component
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { usePathname } from "next/navigation"; 
+
+// Mapping of URL segments to display names of sidebar component
+const pathToNameMap: { [key: string]: string } = {
+  "category-management": "Category Manager",
+  // Add more url element in the sidebar if needed
+};
+
+const generateBreadcrumbItems = (pathname: string) => {
+  const paths = pathname.split("/").filter(Boolean);
+  const breadcrumbItems = paths.map((path, index) => {
+    const href = `/${paths.slice(0, index + 1).join("/")}`; 
+    const label = pathToNameMap[path] || path.replace(/-/g, " "); 
+    return { href, label };
+  });
+  return breadcrumbItems;
+};
+
+export default function DynamicBreadcrumb() {
+  const pathname = usePathname(); 
+  const breadcrumbItems = generateBreadcrumbItems(pathname); 
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {breadcrumbItems.map((item, index) => (
+          <BreadcrumbItem key={index}>
+            <BreadcrumbSeparator />
+            <BreadcrumbLink href={item.href}>
+              {item.label}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
