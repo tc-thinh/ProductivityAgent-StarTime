5a546b1364ebdc4c4790c3204a1100c173308209
tcthinh <congthinh6304@gmail.com>
2025-04-16 12:01:12 -0700

    Reset search results when search query is empty

--- frontend/components/history-dialog/history-dialog.tsx
+++ frontend/components/history-dialog/history-dialog.tsx
@@ -38,6 +38,7 @@ export function HistoryDialog() {
 
         const fetchData = async () => {
             if (!debouncedQuery) {
+                setResult([])
                 return
             }
 
